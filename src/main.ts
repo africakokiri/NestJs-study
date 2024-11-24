aad69760b45c4961bdb32e9919c02dd962783c44
Choi Chan Hwan <africakokiri@gmail.com>
2024-11-24 11:30:04 +0900

    Nestjs Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     snapshot: true,
   });
+  const config = new DocumentBuilder().setTitle('Nest.js Study').build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
