9b34ac0e9e8448f66bf0c657558cd5d6ac74f73a
Choi Chan Hwan <africakokiri@gmail.com>
2024-11-24 11:15:19 +0900

    Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'kokiri',
+      password: 'kokiri',
+      database: 'kokiri',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
